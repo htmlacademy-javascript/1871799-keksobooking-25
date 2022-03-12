55c16e0fbf3016d44fede8328eb8dcc137e8d219
Keks <keks@htmlacademy.ru>
2022-03-12 14:04:22 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/data.js
@@ -0,0 +1,81 @@
+import{getRandomInteger, getRandomFloat, getRandomArrayElement, getRandomArray} from './util.js';
+
+const SIMILAR_OBJECT_COUNT = 10;
+const LAT_NUMBER_MIN = 35.65000;
+const LAT_NUMBER_MAX = 35.70000;
+const LNG_NUMBER_MIN = 139.70000;
+const LNG_NUMBER_MAX = 139.80000;
+const NUMBER_OF_DECIMALS = 5;
+
+const CHECKINS = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const CHECKOUTS = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const TYPES = {
+  'palace': 'Дворец',
+  'flat': 'Квартира',
+  'house': 'Дом',
+  'bungalow': 'Бунгало',
+  'hotel': 'Отель',
+};
+
+const FEATURES_ARRAY = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const PHOTOS_ARRAY = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const MAX_PRICE = 100000;
+const MAX_ROOMS = 5;
+const MAX_GUESTS = 10;
+
+const offerElement = (_elem, id) => {
+  const lat = getRandomFloat(LAT_NUMBER_MIN, LAT_NUMBER_MAX, NUMBER_OF_DECIMALS);
+  const lng = getRandomFloat(LNG_NUMBER_MIN, LNG_NUMBER_MAX, NUMBER_OF_DECIMALS);
+
+  return {
+    author: {
+      avatar: `img/avatars/user${String(++id).padStart(2, '0')}.png`,
+    },
+
+    offer: {
+      title: 'Обьявление о сдаче жилья',
+      address: String(`${lat },${  lng}`),
+      price: getRandomInteger(0, MAX_PRICE),
+      type: getRandomArrayElement(Object.keys(TYPES)),
+      rooms: getRandomInteger(1, MAX_ROOMS),
+      guests: getRandomInteger(1, MAX_GUESTS),
+      checkin: getRandomArrayElement(CHECKINS),
+      checkout: getRandomArrayElement(CHECKOUTS),
+      features: getRandomArray(FEATURES_ARRAY, getRandomInteger (1, FEATURES_ARRAY.length)),
+      description: 'Просторное помещение с прекрасным видом',
+      photos: getRandomArray(PHOTOS_ARRAY, getRandomInteger (1, PHOTOS_ARRAY.length)),
+    },
+
+    location: {
+      lat: lat,
+      lng: lng,
+    }
+  };
+};
+
+const getArrayElement = Array.from({length: SIMILAR_OBJECT_COUNT}, offerElement);
+
+export {getArrayElement, TYPES};
---
+++ 5/js/main.js
@@ -0,0 +1,4 @@
+import {getArrayElement} from './data.js';
+import {getSimilarElements} from './generating-similar-elements.js';
+
+getSimilarElements(getArrayElement[8]);
